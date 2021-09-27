4d7f6567df05758f6e7cc5c6ebef0300928c6744
Diego Martinez <juandiego337@hotmail.com>
2021-09-27 17:21:07 -0500

    Adding behaviors in the pet entity

--- src/main/java/com/training/sofka/veterinaryAppointments/customer/Customer.java
+++ src/main/java/com/training/sofka/veterinaryAppointments/customer/Customer.java
@@ -1,8 +1,8 @@
 package com.training.sofka.veterinaryAppointments.customer;
 
 import co.com.sofka.domain.generic.AggregateEvent;
-import co.com.sofka.domain.generic.Identity;
 import com.training.sofka.veterinaryAppointments.customer.entity.Mascot;
+import com.training.sofka.veterinaryAppointments.customer.event.AddedCustomer;
 import com.training.sofka.veterinaryAppointments.customer.value.CustomerID;
 import com.training.sofka.veterinaryAppointments.customer.value.Email;
 import com.training.sofka.veterinaryAppointments.customer.value.Name;
@@ -17,7 +17,8 @@ public class Customer extends AggregateEvent<CustomerID> {
     protected Telephone telephone;
     protected Set<Mascot> mascots;
 
-    public Customer(CustomerID customerID, Name name, Email email, Mascot mascot) {
+    public Customer(CustomerID customerID, Name name, Email email, Telephone telephone) {
         super(customerID);
+        appendChange(new AddedCustomer(name,email,telephone)).apply();
     }
 }
--- src/main/java/com/training/sofka/veterinaryAppointments/customer/entity/Mascot.java
+++ src/main/java/com/training/sofka/veterinaryAppointments/customer/entity/Mascot.java
@@ -1,11 +1,43 @@
 package com.training.sofka.veterinaryAppointments.customer.entity;
 
 import co.com.sofka.domain.generic.Entity;
+import com.training.sofka.veterinaryAppointments.customer.value.Age;
+import com.training.sofka.veterinaryAppointments.customer.value.AnimalType;
 import com.training.sofka.veterinaryAppointments.customer.value.MascotID;
+import com.training.sofka.veterinaryAppointments.customer.value.State;
+
+import java.util.Objects;
 
 public class Mascot extends Entity<MascotID> {
 
-    public Mascot(MascotID mascotID) {
+    private final AnimalType animalType;
+    private  Age age;
+    private  State state;
+
+    public Mascot(MascotID mascotID, AnimalType animalType, Age age, State state) {
         super(mascotID);
+        this.animalType = animalType;
+        this.age = age;
+        this.state = state;
+    }
+
+    public void updateStatus(State state){
+        this.state = Objects.requireNonNull(state);
+    }
+
+    public void updateAge(Age age){
+        this.age = Objects.requireNonNull(age);
+    }
+
+    public AnimalType animalType() {
+        return animalType;
+    }
+
+    public Age age() {
+        return age;
+    }
+
+    public State state() {
+        return state;
     }
 }
