079db379d3a97b645321787caa42ac4bd0e6f1c1
Diego Martinez <juandiego337@hotmail.com>
2021-09-27 21:59:44 -0500

    Value object State to pet is ok

--- src/main/java/com/training/sofka/veterinaryAppointments/customer/value/State.java
+++ src/main/java/com/training/sofka/veterinaryAppointments/customer/value/State.java
@@ -2,9 +2,40 @@ package com.training.sofka.veterinaryAppointments.customer.value;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class State implements ValueObject<String> {
+
+    private final String value;
+
+    public State(String value) {
+        this.value = Objects.requireNonNull(value);
+
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El estado de la mascota no puede estar vacia");
+        }
+
+        if(this.value.length()>100){
+            throw new IllegalArgumentException("El estado no puede superar los 100 caracteres");
+        }
+
+    }
+
     @Override
     public String value() {
         return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return Objects.equals(value, state.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
