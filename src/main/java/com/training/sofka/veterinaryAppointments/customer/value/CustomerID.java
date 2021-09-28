84553e2f6e94874f448c40efdc42cb66f71f00a7
Diego Martinez <juandiego337@hotmail.com>
2021-09-27 21:53:23 -0500

    Identifiers implemented correctly

--- src/main/java/com/training/sofka/veterinaryAppointments/customer/value/CustomerID.java
+++ src/main/java/com/training/sofka/veterinaryAppointments/customer/value/CustomerID.java
@@ -5,7 +5,14 @@ import co.com.sofka.domain.generic.Identity;
 public class CustomerID extends Identity {
 
     public CustomerID(){
+    }
 
+    public CustomerID(String id){
+        super(id);
+    }
+
+    public static CustomerID of(String id){
+        return new CustomerID(id);
     }
 
 }
--- src/main/java/com/training/sofka/veterinaryAppointments/customer/value/PetID.java
+++ src/main/java/com/training/sofka/veterinaryAppointments/customer/value/PetID.java
@@ -3,4 +3,16 @@ package com.training.sofka.veterinaryAppointments.customer.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class PetID extends Identity {
+
+    public PetID(){
+    }
+
+    public PetID(String id){
+        super(id);
+    }
+
+    public static PetID of(String id){
+        return new PetID(id);
+    }
+
 }
